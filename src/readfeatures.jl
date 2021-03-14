c97407d4d3944587ba3c94b2a1a5f46b455af8df
charlyalizadeh <charlypro1502@outlook.fr>
2021-03-14 17:52:42 +0100

    Rename getfeatures.jl to readfeatures.jl

--- src/readfeatures.jl
+++ src/readfeatures.jl
@@ -24,24 +24,3 @@ function get_features_decomposition(pre_chordal_graph, chordal_graph, nb_added_e
     merge!(features, chordal_graph_features, pre_chordal_graph_features, cliques_features, kernel_graph_features)
     return features
 end
-
-"""
-    export_features_df_to_csv(collection::Mongoc.Collection, path="./data/")
-
-Export the features dataframe of `collection` in a csv file.
-"""
-function export_features_df_to_csv(collection::Mongoc.Collection, path="./data/"; recompute_keys=true)
-    filepath = joinpath(path, "$(collection.name)_features.csv")
-    recompute_keys && Mongoc.drop(collection.database["$(collection.name)_keys"])
-    df = DecompositionDB.getfeaturesdf(collection)
-    symbnames = [Symbol(name) for name in names(df)]
-    instance_name_index = findfirst(isequal(:instance_name), symbnames)
-    symbnames[1], symbnames[instance_name_index] = symbnames[instance_name_index], symbnames[1]
-    if collection.name == "decompositions"
-        nb_added_edges_index = findfirst(isequal(:nb_added_edges), symbnames)
-        symbnames[2], symbnames[nb_added_edges_index] = symbnames[nb_added_edges_index], symbnames[2]
-    end
-    df = df[!, symbnames]
-    sort!(df, [:instance_name])
-    CSV.write(filepath, df)
-end
